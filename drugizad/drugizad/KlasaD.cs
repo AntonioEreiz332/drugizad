59c296bb11c607f46fc8115bd06c600e08c5e8b4
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-06-07 08:56:16 +0200

    Add files via upload

---
+++ drugizad/drugizad/KlasaD.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drugizad
+{
+    class KlasaD
+    {
+        public bool Suprotno(bool vrijdenost)
+        {
+            bool suprotno = !vrijdenost;
+            return suprotno;
+        }
+
+        public int Suprotno(int broj)
+        {
+            int suprotno = broj*-1;
+            return suprotno;
+
+        }
+
+    }
+}
---
+++ drugizad/drugizad/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drugizad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            KlasaD objekt = new KlasaD();
+            bool prvi=true;
+            int drugi=50;
+            prvi = objekt.Suprotno(prvi);
+            drugi = objekt.Suprotno(drugi);
+            Console.WriteLine("Prva= " + prvi);
+            Console.WriteLine("Druga= " + drugi);
+
+            Console.ReadKey();
+
+        }
+    }
+}
